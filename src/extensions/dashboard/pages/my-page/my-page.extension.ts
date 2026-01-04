7a435b9222b3f5e1fb9039caada7691c47196d17
Abiodun Olanlokun <34736423+ah3eyy@users.noreply.github.com>
2026-01-04 23:56:17 +0100

    Initial commit from Create Wix App

---
+++ src/extensions/dashboard/pages/my-page/my-page.extension.ts
@@ -0,0 +1,8 @@
+import { extensions } from '@wix/astro/builders';
+
+export default extensions.dashboardPage({
+  id: '81c7770e-242f-4d63-bd15-76ea0f63c424',
+  title: 'My Page',
+  routePath: '',
+  component: './extensions/dashboard/pages/my-page/my-page.tsx',
+});
